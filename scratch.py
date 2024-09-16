c86def57f0b32e410b4ac28aa6a0ba9fabf87f55
wrozny <wrozny@gmail.com>
2024-09-16 02:09:30 +0200

    Remade scratch classes

--- scratch.py
+++ scratch.py
@@ -1,135 +1,124 @@
 import json
 import os
+from uuid import uuid4
+
 from scratch_constants import *
-from string import ascii_letters
-from random import choice
 
-SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
-BUILD_DIR_PATH = os.path.join(SCRIPT_PATH, "build")
-PROJECT_DIR_PATH = os.path.join(BUILD_DIR_PATH, "project")
 
-LETTERS = [*ascii_letters]
+# SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
+# BUILD_DIR_PATH = os.path.join(SCRIPT_PATH, "build")
+# PROJECT_DIR_PATH = os.path.join(BUILD_DIR_PATH, "project")
 
 
 def generate_uuid():
-    return "".join([choice(LETTERS) for _ in range(16)])
+    return str(uuid4())
 
 
 class Expression:
-    def __init__(self):
-        self.id = generate_uuid()
+    def __init__(self, expression_tokens):
+        self.expression_tokens = expression_tokens
+
+    def parse_single(self):
+        match self.expression_tokens[0][0]:
+            case "STRING":
+                return self.expression_tokens[0][1].strip(r'"')
+            case "NUMBER":
+                return self.expression_tokens[0][1]
+            case "ARGUMENT":
+                return ""
+            case "VARIABLE":
+                return "Variable"
+
+    def parse(self):
+        if len(self.expression_tokens) == 1:
+            return self.parse_single()
+
+        return "complex expression"
 
 
 class Block:
-    def __init__(self, opcode, inputs=None, fields=None):
-        self.opcode = opcode
-        self.inputs = inputs
-        self.fields = fields
+    def __init__(self, opcode, inputs=None, fields=None, is_top_level=False):
+        self.data = BLOCK_BASE.copy()
+        self.data["opcode"] = opcode
+        self.data["inputs"] = inputs or {}
+        self.data["fields"] = fields or {}
+        self.set_level(is_top_level)
         self.id = generate_uuid()
 
-    def compile(self, parent_block=None, next_block=None):
-        block = BLOCK_BASE.copy()
-        block["next"] = next_block
-        block["parent"] = parent_block
-        block["opcode"] = self.opcode
-        block["inputs"] = self.inputs or {}
-        block["fields"] = self.fields or {}
-        return self.id, block
+    def set_next(self, next_block):
+        self.data["next"] = next_block
+
+    def set_parent(self, parent_block):
+        self.data["parent"] = parent_block
+
+    def set_level(self, is_top_level):
+        if is_top_level:
+            self.set_parent(parent_block=None)
+            self.data["x"] = 0
+            self.data["y"] = 0
+            self.data["topLevel"] = is_top_level
+        else:
+            self.data.pop("x", None)
+            self.data.pop("y", None)
+            self.data["topLevel"] = is_top_level
 
 
 class BlockStack:
     def __init__(self):
-        self.blocks = []
+        self._blocks = []
+        self._current_index = -1
 
-    def add_block(self, block):
-        self.blocks.append(block)
+    def add_block(self, block_obj):
+        self._current_index += 1
+        self._blocks.append(block_obj)
+        if len(self._blocks) > 1:
+            previous_block = self._blocks[self._current_index - 1]
+            previous_block.set_next(block_obj.id)
+            block_obj.set_parent(previous_block.id)
 
-    def compile_stack(self, previous_stack_end_block_id=None, next_stack_start_id=None):
-        results = {}
-        previous_block = previous_stack_end_block_id
-        stack_length = len(self.blocks)
-
-        for block_index, block in enumerate(self.blocks):
-            next_block_id = next_stack_start_id
-            if not block.id == self.blocks[stack_length - 1].id:
-                next_block_id = self.blocks[block_index + 1].id
-
-            previous_block, block_data = block.compile(parent_block=previous_block, next_block=next_block_id)
-            if block_index == 0 and previous_stack_end_block_id is None:
-                block_data["topLevel"] = True
-                block_data["x"] = 0
-                block_data["y"] = 0
-
-            results[previous_block] = block_data
-
-        return results
+    def get_blocks(self, last_block=None):
+        if last_block is None:
+            self._blocks[0].set_level(True)
+        else:
+            self._blocks[0].set_parent(last_block.id)
+        return self._blocks
 
 
 class Sprite:
     def __init__(self, name):
         self.name = name
-        self.variables = {}
+        self.data = SPRITE_BASE.copy()
+        self.data["name"] = name
+        self.block_stacks = {}
 
     def get_variable_id(self, name):
-        return name + f"-{self.name}"
+        return f"{self.name}.{name}"
 
     def create_variable(self, name, initial_value):
-        self.variables[self.get_variable_id(name)] = [name, initial_value]
+        variable_id = self.get_variable_id(name)
+        self.data["variables"][variable_id] = [name, initial_value or ""]
 
-    def compile(self):
-        sprite = SPRITE_BASE.copy()
-        sprite["name"] = self.name
-
-        variables = {}
-        for variable_id in self.variables:
-            variable_data = self.variables[variable_id]
-            variable_name = variable_data[0]
-            initial_value = variable_data[1] or ""
-            variables[variable_id] = [variable_name, initial_value]
-
-        sprite["variables"] = variables
-
-        # variable initial setters
-
-        setters_block_stack = BlockStack()
-        setters_block_stack.add_block(Block(opcode="event_whenflagclicked"))
-
-        for variable_name in self.variables:
-            variable_data = self.variables[variable_name]
-            block = Block(
-                opcode="data_setvariableto",
-                inputs={"VALUE": [1, [10, variable_data[1] or ""]]},
-                fields={"VARIABLE": [variable_data[0], self.get_variable_id(variable_data[0])]}
-            )
-            setters_block_stack.add_block(block)
-
-        setters_output = setters_block_stack.compile_stack()
-        for block_id in setters_output:
-            sprite["blocks"][block_id] = setters_output[block_id]
-
-        sprite["blocks"] = setters_output
-        # compile blocks and put them inside the sprite
-        return sprite
+    def set_block(self, block_id, block_data):
+        self.data["blocks"][block_id] = block_data
 
 
 class Project:
     def __init__(self):
-        self.sprites = []
+        self.data = PROJECT_BASE.copy()
+        self.stage = DEFAULT_STAGE.copy()
+        self.data["targets"].append(self.stage)
 
     def add_sprite(self, sprite):
-        self.sprites.append(sprite)
+        self.data["targets"].append(sprite)
 
-    def build_project(self, output_directory=PROJECT_DIR_PATH):
-        project = PROJECT_BASE.copy()
-        project["targets"].append(DEFAULT_STAGE.copy())  # add empty stage to the project
+    def create_variable(self, variable_name, initial_value=None):
+        self.stage["variables"][variable_name] = [variable_name, initial_value or ""]
 
-        for sprite in self.sprites:
-            project["targets"].append(sprite.compile())
-
+    def build_project(self, output_directory):
         if not os.path.exists(output_directory):
             os.makedirs(output_directory)
 
         project_json_path = os.path.join(output_directory, "project.json")
         with open(project_json_path, "w") as output:
-            json_output = json.dumps(project)
+            json_output = json.dumps(self.data)
             output.write(json_output)
